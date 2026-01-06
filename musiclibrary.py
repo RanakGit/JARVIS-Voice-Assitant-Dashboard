655a3e0111ee77b776bf60ab14847f3015400d5e
Ranak Halder <ranakhalder1@gmail.com>
2026-01-06 21:05:42 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth" : "https://youtu.be/U47Tr9BB_wE?si=XYB-CN5vrogSnux7",
+    "electronic" : "https://youtu.be/K4DyBUG242c?si=4a1kcVUJUcV3S8tw",
+    "fearless" : "https://youtu.be/S19UcWdOA-I?si=Wd9boP6CBFQsznTb",
+    "universal" : "https://youtu.be/b56V-KyT9XM?si=sYHphPP-TNjfoS3f"
+}
